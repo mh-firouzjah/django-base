3afc1ddba72dd48d7c89f11762571f473c6e8905
mh-firouzjaah <mh.firouzjah@gmail.com>
2021-08-30 21:53:06 +0430

    tests for user model

--- apps/accounts/tests/tests.py
+++ apps/accounts/tests/tests.py
@@ -1,3 +1,12 @@
+from apps.accounts.models import User
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserModelTests(TestCase):
+    def test_str_method(self):
+        user = User(username='ali')
+        self.assertEqual(str, type(user.get_full_name()))
+        self.assertEqual(user.get_full_name(), user.username)
+        user.first_name = 'Ali'
+        user.last_name = 'Big'
+        self.assertEqual('Ali Big', user.get_full_name())
