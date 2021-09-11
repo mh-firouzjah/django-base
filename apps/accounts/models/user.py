31442bf2bac0e1265fbf2e2b020b4ad884f4a490
mh-firouzjaah <mh.firouzjah@gmail.com>
2021-09-11 23:24:14 +0430

    updates

--- apps/accounts/models/user.py
+++ apps/accounts/models/user.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class User(AbstractUser):
 
     def get_full_name(self):
-        return f"{self.first_name} {self.last_name}".strip() or self.username
+        return super().get_full_name() or self.username
 
     def __str__(self):
         return self.get_full_name()
